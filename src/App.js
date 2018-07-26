a7ba6f93639acb2d0c0b2ea9a64230910e7c2972
unknown <kamilpszczola@gmail.com>
2018-07-26 14:37:49 +0200

    Basic app components, initializing firebase authentication

--- src/App.js
+++ src/App.js
@@ -2,17 +2,28 @@ import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
+import LoginForm from './components/LoginForm';
+
 
 class App extends Component {
+    componentWillMount() {
+        const config = {
+            apiKey: "AIzaSyBkxa3PCUxy1KfYyjYmc9rQpNDi_ZRV5TA",
+            authDomain: "manager-b468e.firebaseapp.com",
+            databaseURL: "https://manager-b468e.firebaseio.com",
+            projectId: "manager-b468e",
+            storageBucket: "",
+            messagingSenderId: "192112065075"
+        };
+        firebase.initializeApp(config);
+    }
+
     render() {
         return (
             <Provider store={createStore(reducers)}>
-                <View>
-                    <Text>
-                        Hello!
-                    </Text>
-                </View>
+                <LoginForm/>
             </Provider>
         )
     }
