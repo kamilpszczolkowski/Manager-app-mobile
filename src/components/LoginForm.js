da441c872bec39e2c533aa09a60383a8b65846d1
unknown <kamilpszczola@gmail.com>
2018-07-27 13:16:25 +0200

    Handling login

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {Card, CardSection, Input, Button} from './common';
+import {Card, CardSection, Input, Button, Spinner} from './common';
 import {emailChanged, passwordChanged, loginUser} from '../actions';
+import {View, Text} from 'react-native';
 
 
 class LoginForm extends Component {
@@ -17,6 +18,29 @@ class LoginForm extends Component {
         const {email, password} = this.props;
 
         this.props.loginUser({email, password});
+    }
+
+    renderError() {
+        if (this.props.error) {
+            return (
+                <View style={{backgroundColor: 'white'}}>
+                    <Text style={styles.errorTextStyle}>
+                        {this.props.error}
+                    </Text>
+                </View>
+            )
+        }
+    }
+
+    renderButton() {
+        if (this.props.loading) {
+            return <Spinner size="large"/>;
+        }
+        return (
+            <Button onPress={this.onButtonPress.bind(this)}>
+                Log In/Sign In
+            </Button>
+        )
     }
 
     render() {
@@ -39,10 +63,10 @@ class LoginForm extends Component {
                         value={this.props.password}
                     />
                 </CardSection>
+
+                {this.renderError()}
                 <CardSection>
-                    <Button onPress={this.onButtonPress.bind(this)}>
-                        Log In/Sign In
-                    </Button>
+                    {this.renderButton()}
                 </CardSection>
             </Card>
         )
@@ -52,10 +76,20 @@ class LoginForm extends Component {
 const mapStateToProps = state => {
     return {
         email: state.auth.email,
-        password: state.auth.password
+        password: state.auth.password,
+        error: state.auth.error,
+        loading: state.auth.loading
     }
 };
 
 export default connect(mapStateToProps, {
     emailChanged, passwordChanged, loginUser
 })(LoginForm);
+
+const styles = {
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red'
+    }
+};
--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,12 +1,17 @@
 import {
     EMAIL_CHANGED,
     PASSWORD_CHANGED,
-    LOGIN_USER_SUCCESS
+    LOGIN_USER_SUCCESS,
+    LOGIN_USER_FAIL,
+    LOGIN_USER_START
 } from "../actions/types";
 
 const INITIAL_STATE = {
     email: '',
-    password: ''
+    password: '',
+    user: null,
+    error: null,
+    loading: false
 }; //
 
 export default (state = INITIAL_STATE, action) => {
@@ -17,8 +22,12 @@ export default (state = INITIAL_STATE, action) => {
             return {...state, email: action.payload}; //It's necessary to cerate new object - don't modify old state and paste it in here
         case PASSWORD_CHANGED:
             return {...state, password: action.payload};
+        case LOGIN_USER_START:
+            return {...state, loading: true, error: ''};
         case LOGIN_USER_SUCCESS:
-            return {...state, user: action.payload};
+            return {...state, ...INITIAL_STATE, user: action.payload}; //Reset all properties in initial state
+        case LOGIN_USER_FAIL:
+            return {...state, error: 'Authentication Failed.', password: '', loading: false};
         default:
             return state;
     }
