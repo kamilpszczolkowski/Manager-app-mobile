39e28860abb1e9de2544805fd7fb49613c72bbb5
unknown <kamilpszczola@gmail.com>
2018-07-27 12:15:34 +0200

    handling user login

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,6 +1,7 @@
 import {
     EMAIL_CHANGED,
-    PASSWORD_CHANGED
+    PASSWORD_CHANGED,
+    LOGIN_USER_SUCCESS
 } from "../actions/types";
 
 const INITIAL_STATE = {
@@ -16,8 +17,9 @@ export default (state = INITIAL_STATE, action) => {
             return {...state, email: action.payload}; //It's necessary to cerate new object - don't modify old state and paste it in here
         case PASSWORD_CHANGED:
             return {...state, password: action.payload};
+        case LOGIN_USER_SUCCESS:
+            return {...state, user: action.payload};
         default:
             return state;
     }
-
 }
