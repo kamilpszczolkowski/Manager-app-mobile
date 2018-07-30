157dcc541fa85639c5a3d1f9a031c8b4ef3b2ff2
unknown <kamilpszczola@gmail.com>
2018-07-30 12:04:21 +0200

    Creating Employee Form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
-import { combineReducers } from 'redux';
+import {combineReducers} from 'redux';
 import AuthReducer from './AuthReducer';
+import EmplyeFormReducer from './EmployeeFormReducer';
 
 export default combineReducers({
-   auth: AuthReducer
+    auth: AuthReducer,
+    employeeForm: EmplyeFormReducer
 });
